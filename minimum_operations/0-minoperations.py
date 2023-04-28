bcc11b6c0484ae6d46ea51d9e9c79b18e0761971
Maximum Fredrick <102311122+MaxEfuru@users.noreply.github.com>
2023-04-28 11:26:59 +0100

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -12,9 +12,9 @@ def minOperations(n):
     exactly n H characters in a text file.
 
     Args:
-        n (int): the desired number of H characters     
+        n (int): the desired number of H characters
     Returns:
-        int: the minimum number of operations needed to achieve n H's 
+        int: the minimum number of operations needed to achieve n H's
         in the file
     Raises:
         None
@@ -28,6 +28,6 @@ def minOperations(n):
         return 0  # invalid input, impossible to achieve
     for i in range(2, n+1):
         if n % i == 0:
-            return minOperations(n//i) + i  
+            return minOperations(n//i) + i
         # recursively compute minimum number of operations
     return 0  # n is prime or 1, impossible to achieve
