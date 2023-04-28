0c82cd5c20c8cbb27a5e16a6cda7be48c181e48b
Maximum Fredrick <102311122+MaxEfuru@users.noreply.github.com>
2023-04-28 11:21:00 +0100

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -8,7 +8,7 @@ Goo
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to result in 
+    Calculates the fewest number of operations needed to result in
     exactly n H characters in a text file.
 
     Args:
